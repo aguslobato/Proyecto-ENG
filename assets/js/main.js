c74a4875349860f2324c9b4e76f2131120067402
Agustin Lobato <85140053+aguslobato@users.noreply.github.com>
2025-05-26 12:28:53 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,16 +15,41 @@ var swiper = new Swiper(".mySwiper", {
         spaceBetween: 10,
       },
       "@0.75": {
-        slidesPerView: 2,
+        slidesPerView: 1,
         spaceBetween: 20,
       },
       "@1.00": {
-        slidesPerView: 3,
+        slidesPerView: 2,
         spaceBetween: 40,
       },
       "@1.50": {
-        slidesPerView: 4,
+        slidesPerView: 3,
         spaceBetween: 50,
       },
     },
   });
+
+
+
+  function toggleDropdown(e) {
+    e.preventDefault();
+    const menu = e.target.closest('.dropdown').querySelector('.dropdown-menu');
+    menu.classList.toggle('show');
+  }
+
+  // Cierra si se hace clic fuera
+  document.addEventListener('click', function (e) {
+    const dropdowns = document.querySelectorAll('.dropdown-menu');
+    dropdowns.forEach(menu => {
+      if (!menu.contains(e.target) && !menu.previousElementSibling.contains(e.target)) {
+        menu.classList.remove('show');
+      }
+    });
+  });
+
+
+
+  function toggleAccordion() {
+    const content = document.getElementById("accordionContent");
+    content.classList.toggle("show");
+  }
